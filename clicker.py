6994456ba27f2f965d4a5f19dcaf00858c036534
wert <fhvfhvb@gmail.com>
2023-04-29 11:31:17 +0500

    Initial commit

---
+++ clicker.py
@@ -0,0 +1,8 @@
+import time,view
+
+import pygame
+
+
+while True:
+    time.sleep(1/100)
+    pygame.event.get()
---
+++ view.py
@@ -0,0 +1,5 @@
+import model
+import pygame
+
+
+pygame.display.set_mode([0,0],pygame.FULLSCREEN)
