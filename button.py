4218d2f5d5cfa195455b59c688f16851732eb0fd
wert <fhvfhvb@gmail.com>
2023-05-27 11:33:44 +0500

    n4.6(отбивка!)

--- button.py
+++ button.py
@@ -3,12 +3,16 @@ import random
 import pygame
 
 
-class Button():
+# otbivka_y = 0
+# otbivka_x = 0
+class Button:
     def __init__(self, a, color, mesto):
         self.a = a
         self.color = color
         self.mesto = mesto
-        self.skorost_rosta = random.randint(1,3)
+        self.skorost_rosta = random.randint(1, 3)
+        self.otbivka_y = 0
+        self.otbivka_x = 0
         print("dxgfdgdgfghxdg")
 
     def plus_piat(self):
@@ -21,7 +25,32 @@ class Button():
         pygame.draw.circle(surface, self.color, self.mesto, self.a)
 
     def rost(self):
+        # global otbivka_y,otbivka_x
         if self.a < 50:
-            self.a += self.skorost_rosta/3
+            self.a += self.skorost_rosta / 3
         else:
             self.a -= 50
+
+        if self.a + self.mesto[1] >= 700:
+            self.otbivka_y = 1
+        if self.mesto[1] - self.a <= 0:
+            self.otbivka_y = 0
+
+        if self.otbivka_y == 0:
+            self.mesto[1] += 1
+
+        if self.otbivka_y == 1:
+            self.mesto[1] -= 1
+
+        if self.a + self.mesto[0] >= 1400:
+            self.otbivka_x = 1
+        if self.mesto[0] - self.a <=0:
+            self.otbivka_x = 0
+
+        if self.otbivka_x == 0:
+            self.mesto[0] += 1
+
+        if self.otbivka_x == 1:
+            self.mesto[0] -= 1
+
+            # self.mesto[0] += 1
